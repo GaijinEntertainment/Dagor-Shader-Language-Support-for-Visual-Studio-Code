42ce730583179d6459321d13acdd6d9a77906f49
zalan_racz <zalan_racz@gaijin.team>
2023-11-14 19:34:54 +0100

    Adding shader config override tests

--- src/test/document-link-resolve.test.ts
+++ src/test/document-link-resolve.test.ts
@@ -33,6 +33,41 @@ suite('Document link resolve include directives in .hlsl files', () => {
     });
 });
 
+suite('Document link resolve override include directives in .sh files', () => {
+    test('should find the included files, based on the override include folders', async () => {
+        await setShaderConfigOverride(`${shadersFolder}/shaders_other.blk`);
+
+        const uri = getDocumentUri(`${shadersFolder}/test.sh`);
+        await openDocumentAndAssertLinks(uri, [
+            'folder_2/test_inc_3.hlsl', //  #include "test_inc_3.hlsl"
+            'test_inc_1.hlsl', //           #include "test_inc_1.hlsl"
+            'test_inc_1.hlsl', //           #include <test_inc_1.hlsl>
+            'test_inc_1.hlsl', //           #include <../test_inc_1.hlsl>
+            'test_inc_1.sh', //             include "test_inc_1.sh"
+            'folder_2/test_inc_3.sh', //    include "test_inc_3.sh"
+            'test_inc_2.sh', //             include_optional "test_inc_2.sh"
+        ]);
+
+        await setShaderConfigOverride('');
+    });
+});
+
+suite('Document link resolve include directives in .hlsl files', () => {
+    test('should find the included files, based on the include folders', async () => {
+        await setShaderConfigOverride(`${shadersFolder}/shaders_other.blk`);
+
+        const uri = getDocumentUri(`${shadersFolder}/test.hlsl`);
+        await openDocumentAndAssertLinks(uri, [
+            'folder_2/test_inc_3.hlsl', //  #include "test_inc_3.hlsl"
+            'test_inc_1.hlsl', //           #include "test_inc_1.hlsl"
+            'test_inc_1.hlsl', //           #include <test_inc_1.hlsl>
+            'test_inc_1.hlsl', //           #include <../test_inc_1.hlsl>
+        ]);
+
+        await setShaderConfigOverride('');
+    });
+});
+
 async function openDocumentAndAssertLinks(
     uri: vscode.Uri,
     expectedLinks: string[]
@@ -51,4 +86,10 @@ async function openDocumentAndAssertLinks(
         const expectedItem = path.normalize(`${shadersFolder}/${expectedLink}`);
         assert.ok(actualItem.endsWith(expectedItem));
     });
+}
+
+async function setShaderConfigOverride(value: string): Promise<void> {
+    await vscode.workspace
+        .getConfiguration('dagorShaderLanguageServer')
+        .update('shaderConfigOverride', value);
 }
