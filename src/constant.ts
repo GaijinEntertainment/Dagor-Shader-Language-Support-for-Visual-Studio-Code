96c611f8862e2878851339c8ad31c223a360566d
zalan_racz <zalan_racz@gaijin.team>
2024-01-25 13:11:37 +0100

    Adding a context menu item to the shader config files to make shader config overriding easier

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,7 @@
 export const EXTENSION_ID = 'dagor-shader-language-support';
 export const EXTENSION_NAME = 'Dagor Shader Language Support';
 export const EXTENSION_PUBLISHER = 'gaijin';
+
+export const LANGUAGE_SERVER_ID = 'dagorShaderLanguageServer';
+export const COMMAND_ID_SET_SHADER_CONFIG = `${LANGUAGE_SERVER_ID}.setShaderConfig`;
+export const CONFIGURATION_ID_SHADER_CONFIG_OVERRIDE = 'shaderConfigOverride';
